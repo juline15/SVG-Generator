41e2aa572ba1aace2e9e894354c7becb345b405e
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2025-10-09 08:25:17 +0200

    pbase

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from "./pocketbase-types";
 var path = '';
 if (import.meta.env.MODE === 'development')
-    path = 'http://localhost:8086'    //localhost = machine de dev
+    path = 'http://localhost:8090'    //localhost = machine de dev
 else path = 'http://localhost:8086'   //localhost = machine de déploiement
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb
